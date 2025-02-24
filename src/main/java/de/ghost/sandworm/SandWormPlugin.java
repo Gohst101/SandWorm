25c8e3323ba33b18b7acc7fefa610880c2e9a620
Ghost101 <hhamjediers@gmail.com>
2025-02-24 18:46:51 +0100

    Update Plugin

--- src/main/java/de/ghost/sandworm/SandWormPlugin.java
+++ src/main/java/de/ghost/sandworm/SandWormPlugin.java
@@ -1,18 +1,13 @@
 package de.ghost.sandworm;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
-import org.apache.commons.lang3.BitField;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Mob;
-import org.bukkit.entity.Zombie;
-import org.bukkit.entity.Player;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.boss.BossBar;
 import org.bukkit.boss.BarColor;
@@ -20,20 +15,19 @@ import org.bukkit.boss.BarStyle;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.CreatureSpawnEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
-public class SandWormPlugin extends JavaPlugin implements Listener {
+public class SandWormPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
+        getServer().getPluginManager().registerEvents(new org.bukkit.event.Listener() {}, this);
         getLogger().info("Sand Worm Plugin Enabled!");
     }
 
@@ -42,77 +36,45 @@ public class SandWormPlugin extends JavaPlugin implements Listener {
         getLogger().info("Sand Worm Plugin Disabled!");
     }
 
-
     public void spawnSandWorm(Location location) {
         World world = location.getWorld();
         if (world == null) return;
 
-
-/*
-        Location location01 = new Location(Bukkit.getWorld("world"), 100, 65, 200);
-        World world01 = location01.getWorld();
-        world01.spawnEntity(location, EntityType.ZOMBIE);
-*/
-
-
-        // Coordinaten des Spielers
-        Location initialLocation;
-        initialLocation = location.clone().add(0, 0, 0);
-
-        // Verwende einen Zombie als Basis
-        Mob sandWorm = (Mob) world.spawnEntity(initialLocation, EntityType.ZOMBIE);
-        sandWorm.setCustomName("Sand Worm");
-        sandWorm.setCustomNameVisible(true);
-        sandWorm.getAttribute(Attribute.MAX_HEALTH).setBaseValue(200.0);
-        sandWorm.setHealth(200.0);
-        sandWorm.getAttribute(Attribute.ATTACK_DAMAGE).setBaseValue(15.0);
-        sandWorm.getAttribute(Attribute.MOVEMENT_SPEED).setBaseValue(0.25);
-
-        // Unterdrücke alle Geräusche
-        sandWorm.setSilent(true);
-
-
-        // Füge den Tag "sandworm" zur Identifikation hinzu
-        sandWorm.addScoreboardTag("sandworm");
-
-        // Stelle sicher, dass nur große Zombies gespawnt werden und das Monster nicht despawnt:
-        if (sandWorm instanceof Zombie) {
-            Zombie zombie = (Zombie) sandWorm;
-            zombie.setBaby(false);                     // Nur große Zombies
-            zombie.setRemoveWhenFarAway(false);          // Nicht despawnen, wenn der Chunk entladen wird
+        Location initialLocation = location.clone();
+        ArmorStand head = (ArmorStand) world.spawnEntity(initialLocation, EntityType.ARMOR_STAND);
+        head.setCustomName("Sand Worm");
+        head.setCustomNameVisible(true);
+        head.setGravity(false);
+        if (head.getAttribute(Attribute.MAX_HEALTH) != null) {
+            head.getAttribute(Attribute.MAX_HEALTH).setBaseValue(200.0);
+            head.setHealth(200.0);
         }
+        head.setSilent(true);
+        head.addScoreboardTag("sandworm");
 
-        // Hole Coordinaten vom Sandwurm - Coordinate Y
-        double normalHeight = sandWorm.getLocation().getY();
+        double normalHeight = head.getLocation().getY();
         Bukkit.broadcastMessage("Sand Worm spawn Y-Koordinate: " + normalHeight);
         double underGroundHeight = normalHeight - 10;
 
-        // Erstelle eine gelbe Bossbar für diesen Sand Worm
         BossBar bossBar = Bukkit.createBossBar("Sand Worm Health", BarColor.YELLOW, BarStyle.SOLID);
-        bossBar.setProgress(sandWorm.getHealth() / sandWorm.getAttribute(Attribute.MAX_HEALTH).getBaseValue());
+        bossBar.setProgress(head.getHealth() / head.getAttribute(Attribute.MAX_HEALTH).getBaseValue());
         for (Player player : Bukkit.getOnlinePlayers()) {
             bossBar.addPlayer(player);
         }
 
-        // Aktualisiere die Bossbar jede Sekunde
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                if (!sandWorm.isValid() || sandWorm.getHealth() <= 0 || !sandWorm.getScoreboardTags().contains("sandworm")) {
-                    bossBar.removeAll();
-                    this.cancel();
-                    return;
-                }
-                double progress = sandWorm.getHealth() / sandWorm.getAttribute(Attribute.MAX_HEALTH).getBaseValue();
-                bossBar.setProgress(progress);
-            }
-        }.runTaskTimer(this, 0, 20);
 
-        // Erstelle eine Liste von 13 Zielpositionen relativ zur Spawnposition (base)
+        List<ArmorStand> snakeParts = new ArrayList<>();
+        snakeParts.add(head);
+        LinkedList<Location> headTrail = new LinkedList<>();
+        headTrail.add(head.getLocation().clone());
+
+        final int delayFactor = 4;
+        final int maxLength = 30;
+
         Location base = location.clone();
         List<Location> targetPositions = new ArrayList<>();
         targetPositions.add(new Location(world, base.getX() - 75, base.getY(), base.getZ() - 75)); // 0
-        targetPositions.add(new Location(world, base.getX(), base.getY(), base.getZ() - 75)); // 1
+        targetPositions.add(new Location(world, base.getX(), base.getY(), base.getZ() - 75));      // 1
         targetPositions.add(new Location(world, base.getX() + 75, base.getY(), base.getZ() - 75)); // 2
         targetPositions.add(new Location(world, base.getX() - 35, base.getY(), base.getZ() - 35)); // 3
         targetPositions.add(new Location(world, base.getX() + 35, base.getY(), base.getZ() - 35)); // 4
@@ -122,72 +84,119 @@ public class SandWormPlugin extends JavaPlugin implements Listener {
         targetPositions.add(new Location(world, base.getX() - 35, base.getY(), base.getZ() + 35)); // 8
         targetPositions.add(new Location(world, base.getX() + 35, base.getY(), base.getZ() + 35)); // 9
         targetPositions.add(new Location(world, base.getX() - 75, base.getY(), base.getZ() + 75)); // 10
-        targetPositions.add(new Location(world, base.getX(), base.getY(), base.getZ() + 75)); // 11
+        targetPositions.add(new Location(world, base.getX(), base.getY(), base.getZ() + 75));      // 11
         targetPositions.add(new Location(world, base.getX() + 75, base.getY(), base.getZ() + 75)); // 12
 
-        // Setze initial das Ziel auf die Spawnposition (Index 6)
         final Location[] currentTarget = new Location[]{ targetPositions.get(6) };
 
-        // Bewege den Zombie kontinuierlich in Richtung des aktuellen Ziels ("hingleiten")
         new BukkitRunnable() {
+            Vector currentDirection = currentTarget[0].toVector().subtract(initialLocation.toVector()).normalize();
+            int tickCount = 0;
             @Override
             public void run() {
-                if (!sandWorm.isValid()) {
+                if (!snakeParts.get(0).isValid() || snakeParts.get(0).getHealth() <= 0 ||
+                        !snakeParts.get(0).getScoreboardTags().contains("sandworm")) {
+                    bossBar.removeAll();
                     this.cancel();
                     return;
                 }
 
-                // Change Code from here
-
-                Location currentLoc = sandWorm.getLocation();
-                Location targetLoc = currentTarget[0].clone();  // Clone des Zielortes
-
-                // Hier den Y-Wert angleichen, damit nur X und Z berücksichtigt werden:
+                ArmorStand head = snakeParts.get(0);
+                Location currentLoc = head.getLocation();
+                Location targetLoc = currentTarget[0].clone();
                 targetLoc.setY(currentLoc.getY());
-
-
                 double distance = currentLoc.distance(targetLoc);
-                if (distance > 0.5) {
-                    Vector direction = targetLoc.toVector().subtract(currentLoc.toVector());
-                    direction.normalize();
-                    // Verwende die normale Laufgeschwindigkeit (vom Attribut)
-                    double speed = sandWorm.getAttribute(Attribute.MOVEMENT_SPEED).getBaseValue();
-                    direction.multiply(speed);
-                    sandWorm.setVelocity(direction);
-                } else {
-                    // Ziel erreicht
-                    sandWorm.setVelocity(new Vector(0, 0, 0));
-                    Bukkit.broadcastMessage("[DEBUG] Sand Worm: Ziel erreicht: " + targetLoc.toVector().toString());
+                double speed = 0.25;
 
-                    // Generiere eine neue zufällige Y-Koordinate (entweder ~-20 oder ~)
+                if (distance > 0.5) {
+                    Vector desiredDirection = targetLoc.toVector().subtract(currentLoc.toVector()).normalize();
+                    double maxTurnAngle = Math.toRadians(2);
+                    double angleDiff = currentDirection.angle(desiredDirection);
+                    if (angleDiff > maxTurnAngle) {
+                        Vector axis = currentDirection.clone().crossProduct(desiredDirection);
+                        if (axis.length() != 0) {
+                            axis.normalize();
+                            currentDirection = rotateVector(currentDirection, axis, maxTurnAngle);
+                        } else {
+                            currentDirection = desiredDirection;
+                        }
+                    } else {
+                        currentDirection = desiredDirection;
+                    }
+                    Vector move = currentDirection.clone().multiply(speed);
+                    Location newLoc = currentLoc.clone().add(move);
+                    head.teleport(newLoc);
+                } else {
+                    head.teleport(currentLoc);
+                    Bukkit.broadcastMessage("[DEBUG] Sand Worm: Ziel erreicht: " + targetLoc.toVector().toString());
                     int randomLocationType = ThreadLocalRandom.current().nextInt(0, 100);
-                    if (randomLocationType < 20) { // 20% Chance unter dem Boden (~-20)
+                    if (randomLocationType < 20) {
                         Location newLocation = currentLoc.clone();
-                        newLocation.setY(underGroundHeight); // unterGroundHeight sollte z. B. -20 sein
-                        sandWorm.teleport(newLocation);
+                        newLocation.setY(underGroundHeight);
+                        head.teleport(newLocation);
                         Bukkit.broadcastMessage("[DEBUG] Sand Worm teleportiert sich unter den Boden: Y = " + underGroundHeight);
-                    } else { // 80% Chance auf normale Höhe (~)
+                    } else {
                         Location newLocation = currentLoc.clone();
-                        newLocation.setY(normalHeight); // normalHeight könnte z. B. 236 sein
-                        sandWorm.teleport(newLocation);
+                        newLocation.setY(normalHeight);
+                        head.teleport(newLocation);
                         Bukkit.broadcastMessage("[DEBUG] Sand Worm bleibt auf normaler Höhe: Y = " + normalHeight);
                     }
-
-                    // Wähle ein neues Ziel aus der Liste
                     int randomIndex = ThreadLocalRandom.current().nextInt(0, targetPositions.size());
                     currentTarget[0] = targetPositions.get(randomIndex);
                     Bukkit.broadcastMessage("[DEBUG] Sand Worm: Neuer Zielpunkt ausgewählt: Index "
                             + randomIndex + " -> " + currentTarget[0].toVector().toString());
+                }
 
+                double progress = head.getHealth() / head.getAttribute(Attribute.MAX_HEALTH).getBaseValue();
+                bossBar.setProgress(progress);
 
+                tickCount++;
+                headTrail.add(head.getLocation().clone());
+                int maxTrailSize = maxLength * delayFactor + 1;
+                if (headTrail.size() > maxTrailSize) {
+                    headTrail.removeFirst();
+                }
 
-                    // To here
+                for (int i = 1; i < snakeParts.size(); i++) {
+                    int index = headTrail.size() - 1 - (i * delayFactor);
+                    if (index >= 0) {
+                        Location targetPos = headTrail.get(index);
+                        snakeParts.get(i).teleport(targetPos);
+                    }
+                }
+
+                if (snakeParts.size() < maxLength) {
+                    int requiredTrailLength = snakeParts.size() * delayFactor + 1;
+                    if (headTrail.size() >= requiredTrailLength) {
+                        int newSegmentIndex = headTrail.size() - 1 - (snakeParts.size() * delayFactor);
+                        if (newSegmentIndex >= 0) {
+                            Location spawnLoc = headTrail.get(newSegmentIndex);
+                            ArmorStand newSegment = (ArmorStand) head.getWorld().spawnEntity(spawnLoc, EntityType.ARMOR_STAND);
+                            newSegment.setCustomName("Sand Worm");
+                            newSegment.setCustomNameVisible(true);
+                            newSegment.setGravity(false);
+                            if (newSegment.getAttribute(Attribute.MAX_HEALTH) != null) {
+                                newSegment.getAttribute(Attribute.MAX_HEALTH).setBaseValue(200.0);
+                                newSegment.setHealth(200.0);
+                            }
+                            newSegment.setSilent(true);
+                            newSegment.addScoreboardTag("sandworm");
+                            snakeParts.add(newSegment);
+                        }
+                    }
                 }
             }
         }.runTaskTimer(this, 0, 1);
     }
 
-    // Command Handling
+    private Vector rotateVector(Vector vector, Vector axis, double angle) {
+        double cos = Math.cos(angle);
+        double sin = Math.sin(angle);
+        return vector.clone().multiply(cos)
+                .add(axis.clone().crossProduct(vector).multiply(sin))
+                .add(axis.clone().multiply(axis.dot(vector) * (1 - cos)));
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("spawnsandworm")) {
